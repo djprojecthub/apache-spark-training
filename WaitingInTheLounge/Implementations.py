0e11843791dc132f1155ffb6da0f9ec1a1c4a04a
djprojecthub <djprojecthub>
2023-07-26 07:11:32 +0000

    implementation day 3 first commit
    parquet data read write

--- WaitingInTheLounge/Implementations.py
+++ WaitingInTheLounge/Implementations.py
@@ -4,7 +4,7 @@
 
 # COMMAND ----------
 
-csvfilepath = '/FileStore/tables/customer_dataset-2.csv' # file stored in DBFS
+csvfilepath = '/FileStore/customer_dataset.csv' # file stored in DBFS
 customer_df = spark.read.csv(
     path=csvfilepath,
     header=True,
@@ -20,7 +20,7 @@ customer_df.show()
 
 # COMMAND ----------
 
-jsonfilepath = '/FileStore/tables/car_owner.json'
+jsonfilepath = '/FileStore/car_owner.json'
 orderDetails_df = (
     spark.read.format('json')
               .option('multiline','true')
@@ -28,3 +28,117 @@ orderDetails_df = (
               .load(jsonfilepath)
 )
 display(orderDetails_df)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### 3. Reading data from a Parquet file into a PySpark Dataframe
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC #### What is Apache Parquet?
+# MAGIC Apache Parquet is a binary file format that stores data in a columnar fashion. Data inside a Parquet file is similar to an RDBMS style table where you have columns and rows. But instead of accessing the data one row at a time, you typically access it one column at a time.
+# MAGIC
+# MAGIC Apache Parquet is one of the modern big data storage formats. It has several advantages, some of which are:
+# MAGIC
+# MAGIC   > Columnar storage: efficient data retrieval, efficient compression, etc...<br/>
+# MAGIC   > Metadata is at the end of the file: allows Parquet files to be generated from a stream of data. (common in big data scenarios)<br/>
+# MAGIC   > Supported by all Apache big data products
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ##### Create dataframe to bo written to parquet file
+
+# COMMAND ----------
+
+data = [
+    ("Diwakar", "", "Joshi", "20-09-1994", "M", 3000),
+    ("Piyush", "Kumar", "Tiwari", "26-05-1994", "M", 3500),
+    ("Lalit", "Mohan", "Tiwari", "13-12-1995", "M", 4000),
+    ("Harleen", "", "Sandhu", "01-01-1993", "F", 2000),
+    ("Simran", "", "Kaur", "12-10-1993", "F", 2000)
+]
+columns = ['FirstName', 'MiddleName', 'LastName', 'DOB', 'Gender', 'Salary']
+
+userdata_df = spark.createDataFrame(data, columns)
+display(userdata_df)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ##### Write dataframe to parquet file
+
+# COMMAND ----------
+
+parquetfilepath = '/FileStore/tables/user_details.parquet'
+
+### 3 ways to right dataframe to parquet file (run any one at a time) ###
+
+### 1. Write dataframe to parquet file ###
+
+# userdata_df.write.parquet(parquetfilepath)
+
+### 2. Write dataframe to parquet file with write mode overwrite ###
+
+userdata_df.write\
+    .format('parquet')\
+        .mode('overwrite')\
+            .save(parquetfilepath)    
+
+### 3. Write dataframe to parquet file with write mode append ###
+
+# userdata_df.write\
+#     .format('parquet')\
+#         .mode('append')\
+#             .save(parquetfilepath)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ##### Read parquet file
+
+# COMMAND ----------
+
+userdataparquetDF = spark.read.parquet(parquetfilepath)
+userdataparquetDF.show()
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ##### Create parquet partition file
+
+# COMMAND ----------
+
+parquetfilepath = '/FileStore/tables/user_details_with_partition.parquet'
+userdata_df.write.partitionBy("gender","salary").mode("overwrite").save(parquetfilepath)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### Read partitioned parquet file
+
+# COMMAND ----------
+
+spark.sql("SET spark.databricks.delta.formatCheck.enabled=false")
+
+# COMMAND ----------
+
+# run cmd 16 to read partitioned data using parquet format
+userdatapartitionedparquetDF = spark.read.parquet(parquetfilepath+'/Gender=M')
+userdatapartitionedparquetDF.show()
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### 4. Reading data from a MySQL database into a PySpark Dataframe
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### 5. Reading data from a web API into a PySpark Dataframe
+
+# COMMAND ----------
+
+
